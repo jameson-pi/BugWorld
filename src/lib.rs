3a1e9669298a41292bd216b917db1ae70de4676d
Jameson Piscitello <75795824+jameson-pi@users.noreply.github.com>
2024-01-07 14:02:44 -0600

    make work

--- src/lib.rs
+++ src/lib.rs
@@ -1,71 +1,90 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
 enum Direction {
     North,
     South,
     East,
-    West
+    West,
 }
+
 struct Cell {
-    north_cell: Option<Box<Cell>>,
-    south_cell: Option<Box<Cell>>,
-    east_cell: Option<Box<Cell>>,
-    west_cell: Option<Box<Cell>>
+    north_cell: Option<Rc<RefCell<Cell>>>,
+    south_cell: Option<Rc<RefCell<Cell>>>,
+    east_cell: Option<Rc<RefCell<Cell>>>,
+    west_cell: Option<Rc<RefCell<Cell>>>,
+    bug: bool,
 }
+
 impl Cell {
-    fn get_neighbor(&self, direction: Direction) -> Option<&Cell> {
+    fn get_neighbor(&self, direction: Direction) -> Option<Rc<RefCell<Cell>>> {
         match direction {
-            Direction::North => self.north_cell.as_deref(),
-            Direction::South => self.south_cell.as_deref(),
-            Direction::East => self.east_cell.as_deref(),
-            Direction::West => self.west_cell.as_deref(),
+            Direction::North => self.north_cell.clone(),
+            Direction::South => self.south_cell.clone(),
+            Direction::East => self.east_cell.clone(),
+            Direction::West => self.west_cell.clone(),
         }
     }
 }
+
 struct Arena {
-    cells: Vec<Cell>,
+    cells: Vec<Rc<RefCell<Cell>>>,
     width: usize,
     height: usize,
 }
 
 impl Arena {
     fn new(width: usize, height: usize) -> Self {
-        let mut cells = Vec::with_capacity(width * height);
+        let mut cells: Vec<Rc<RefCell<Cell>>> = Vec::with_capacity(width * height);
         for _ in 0..width * height {
-            cells.push(Cell {
+            cells.push(Rc::new(RefCell::new(Cell {
                 north_cell: None,
                 south_cell: None,
                 east_cell: None,
                 west_cell: None,
-            });
+                bug: false,
+            })));
         }
 
         // Link cells together for a 2D grid structure
         for y in 0..height {
             for x in 0..width {
-                let index = y * width + x;
-                let cell = &mut cells[index];
-
+                let index: usize = y * width + x;
+                let cell = cells[index].clone();
                 if y > 0 {
-                    cell.north_cell = Some(cells[index - width]);
+                    let north_index = index - width;
+                    let north_cell = cells[north_index].clone();
+                    cell.borrow_mut().north_cell = Some(north_cell);
                 }
                 if y < height - 1 {
-                    cell.south_cell = Some(cells[index + width]);
+                    let south_index = index + width;
+                    let south_cell = cells[south_index].clone();
+                    cell.borrow_mut().south_cell = Some(south_cell);
                 }
                 if x > 0 {
-                    cell.west_cell = Some(cells[index - 1]);
+                    let west_index = index - 1;
+                    let west_cell = cells[west_index].clone();
+                    cell.borrow_mut().west_cell = Some(west_cell);
                 }
                 if x < width - 1 {
-                    cell.east_cell = Some(cells[index + 1]);
+                    let east_index = index + 1;
+                    let east_cell = cells[east_index].clone();
+                    cell.borrow_mut().east_cell = Some(east_cell);
                 }
             }
         }
 
-        Self { cells, width, height }
+        Self {
+            cells,
+            width,
+            height,
+        }
     }
 
     // Access a cell at a given (x, y) coordinate
-    fn get_cell(&self, x: usize, y: usize) -> Option<&Cell> {
+    fn get_cell(&self, x: usize, y: usize) -> Option<Rc<RefCell<Cell>>> {
         if x < self.width && y < self.height {
-            Some(&self.cells[y * self.width + x])
+            Some(self.cells[y * self.width + x].clone())
         } else {
             None
         }
@@ -74,6 +93,6 @@ impl Arena {
 
 struct Bug {
     direction: Direction,
-    position: Cell,
-    is_alive: bool
+    position: Rc<RefCell<Cell>>,
+    is_alive: bool,
 }
